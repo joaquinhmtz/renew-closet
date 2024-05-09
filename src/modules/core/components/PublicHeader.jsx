a7f0de1dc0d40f16e3d1467ecb18133f7ee38ac2
Joaquín Hernández <joaquin_downhill@hotmail.com>
2024-05-08 20:27:00 -0600

    sidenav with option styles

--- src/modules/core/components/PublicHeader.jsx
+++ src/modules/core/components/PublicHeader.jsx
@@ -16,6 +16,7 @@ const icons = [{
 const PublicHeader = () => {
     
     const [top, setTop] = useState(true);
+    const [nav, setNav] = useState(false);
 
     useEffect(() => {
         const scrollHandler = () => {
@@ -29,7 +30,9 @@ const PublicHeader = () => {
         <>
             <nav className={`w-full fixed top-0 z-20 transition-all ${!top && `bg-white shadow-lg`} h-20 py-5 px-10 flex place-items-center justify-between text-primary-black`}>
                 <div className="flex place-items-center">
-                    <Icon key={"faBars"} name={"faBars"} styleClass={"lg:hidden"}/>
+                    <button onClick={() => setNav(!nav)} className="z-35">
+                        <Icon key={"faBars"} name={"faBars"} styleClass={"lg:hidden"}/>
+                    </button>
                     <span className="text-2xl font-semibold invisible lg:visible">Renew-Closet</span>
                 </div>
                 <div className="flex space-x-8">
@@ -40,6 +43,39 @@ const PublicHeader = () => {
                     )}
                 </div>
             </nav>
+
+            {nav ? (
+                <div className="bg-black/80 fixed w-full h-screen z-10 top-0 left-0"></div>
+            ) : (
+                ""
+            )}
+
+            <div 
+                className={ 
+                    nav ? "fixed top-0 left-0 w-[300px] h-screen bg-white z-30 duration-300"
+                    : "fixed top-0 left-[-100%] w-[300px] h-screen bg-white z-30 duration-300"
+                }
+            >
+                <div className="w-full h-20 py-5 px-6 flex flex-col justify-start">
+                    <button onClick={() => setNav(!nav)} className="z-35 self-start py-3">
+                        <Icon key={"faXmark"} name={"faXmark"} styleClass={"lg:hidden"}/>
+                    </button>
+                    <nav>
+                        <ul className="flex flex-col pt-4 text-gray-800">
+                            <li className="w-full px-4 py-4 flex place-items-center hover:bg-gray-100 hover:rounded-md hover:font-medium active:bg-gray-200 cursor-pointer">
+                                <Link to="/">
+                                    <Icon name={"faHouse"} styleClass={"pr-2"}/> Home
+                                </Link>
+                            </li>
+                            <li className="w-full px-4 py-4 flex place-items-center hover:bg-gray-100 hover:rounded-md hover:font-medium active:bg-gray-200 cursor-pointer">
+                                <Link to="/">
+                                    <Icon name={"faHouse"} styleClass={"pr-2"}/> About
+                                </Link>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
         </>
     )
 }
--- src/modules/shared/components/Icon.jsx
+++ src/modules/shared/components/Icon.jsx
@@ -1,5 +1,5 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { faCircleUser ,faHeart, faBagShopping, faBars } from "@fortawesome/free-solid-svg-icons";
+import { faCircleUser ,faHeart, faBagShopping, faBars, faXmark, faHouse } from "@fortawesome/free-solid-svg-icons";
 
 const Icon = ({ name, styleClass }) => {
 
@@ -7,6 +7,8 @@ const Icon = ({ name, styleClass }) => {
     if (name === "faHeart" ? name = faHeart : null);
     if (name === "faBagShopping" ? name = faBagShopping : null);
     if (name === "faBars" ? name = faBars : null);
+    if (name === "faXmark" ? name = faXmark : null);
+    if (name === "faHouse" ? name = faHouse : null);
 
     return (
         <FontAwesomeIcon className={`text-primary-black ${styleClass} hover:text-neutral-400 cursor-pointer active:text-neutral-600`} icon={name} size="xl" />

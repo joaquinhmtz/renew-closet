f56c25ac225713109a6b2051e2a9ff6457e546f3
Joaquín Hernández <joaquin_downhill@hotmail.com>
2024-05-09 12:16:09 -0600

    image product and card responsive

--- src/modules/products/components/public/ProductCard.jsx
+++ src/modules/products/components/public/ProductCard.jsx
@@ -1,8 +1,12 @@
+import imgProduct from "./../../../../assets/t-shirt.png";
+
 const ProductCard = () => {
     return (
         <>
             <div className="w-full flex flex-col mt-12">
-                <div className="w-full min-h-[300px] bg-gray-400">div de imagen</div>
+                <div className="w-full min-h-80 bg-gray-400">
+                    <img className="w-full h-full object-cover" src={imgProduct} alt="profile-picture" />
+                </div>
                 <div>
                     <h2>nombre dle producto</h2>
                     <span>statyus</span>

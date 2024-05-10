a843a8eb374b216086ee831959be818f4d5e5511
Joaquín Hernández <joaquin_downhill@hotmail.com>
2024-05-10 10:39:48 -0600

    text sm in text footer

--- src/modules/core/components/PublicFooter.jsx
+++ src/modules/core/components/PublicFooter.jsx
@@ -2,7 +2,7 @@ const PublicFooter = () => {
     return (
         <>
             <div className="w-full flex justify-center content-center py-5 px-10 bg-white drop-shadow-2xl mt-10">
-                <p className="font-medium">Renew-closet@2024</p>
+                <p className="font-medium text-sm">Renew-closet@2024</p>
             </div>
         </>
     )

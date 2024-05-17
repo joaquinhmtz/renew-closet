1459953f0f0ac11c792147bf416799c9526a6ca7
Joaquín Hernández <joaquin_downhill@hotmail.com>
2024-05-17 15:09:29 -0600

    setting footer

--- src/modules/core/components/PublicFooter.jsx
+++ src/modules/core/components/PublicFooter.jsx
@@ -1,8 +1,8 @@
 const PublicFooter = () => {
     return (
         <>
-            <div className="w-full fixed inset-x-0 bottom-0 flex justify-center content-center py-5 px-10 bg-white drop-shadow-2xl mt-10">
-                <p className="font-medium text-sm">Renew-closet@2024</p>
+            <div className="w-full fixed inset-x-0 bottom-0 flex justify-center content-center py-3.5 px-10 bg-white drop-shadow-2xl mt-10">
+                <p className="font-medium text-xs">Renew-closet@2024</p>
             </div>
         </>
     )
